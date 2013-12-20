af28c5594776d169943a14069d64ecd0763213d1
Joel Verhagen <joel.verhagen@gmail.com>
2013-12-19 21:17:39 -0500

    Log exceptions

--- DataChecker/Program.cs
+++ DataChecker/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using ManyConsole;
+using NLog;
 
 namespace OdjfsScraper.DataChecker
 {
     internal class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public static int Main(string[] args)
         {
-            IEnumerable<ConsoleCommand> commands = ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof (Program));
-            return ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+            try
+            {
+                IEnumerable<ConsoleCommand> commands = ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(Program));
+                return ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+            }
+            catch (Exception e)
+            {
+                Logger.Trace(e.Message);
+                Logger.ErrorException("An error occurred during the execution of OdjfsScraper.DataChecker.", e);
+                return 1;
+            }
         }
     }
 }
