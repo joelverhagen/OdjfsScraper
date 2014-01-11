b79a58fa084bc202a605b8bdec84719596527593
Joel Verhagen <joel.verhagen@gmail.com>
2014-01-11 18:16:09 -0500

    Start work on readers

---
+++ Scraper/Readers/ChildCareReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using NLog;
+using OdjfsScraper.Model.ChildCares;
+using OdjfsScraper.Model.ChildCareStubs;
+using OdjfsScraper.Model.Support;
+using OdjfsScraper.Scraper.Parsers;
+
+namespace OdjfsScraper.Scraper.Readers
+{
+    public class ChildCareReader : IChildCareFetcher
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly IChildCareParser _parser;
+
+        public ChildCareReader(IChildCareParser parser)
+        {
+            _parser = parser;
+        }
+
+        public Task<ChildCare> Fetch(ChildCare childCare)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ChildCare> Fetch(ChildCareStub childCareStub)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Scraper/Readers/ChildCareStubListReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OdjfsScraper.Model;
+using OdjfsScraper.Model.ChildCareStubs;
+using OdjfsScraper.Model.Support;
+using OdjfsScraper.Scraper.Parsers;
+
+namespace OdjfsScraper.Scraper.Readers
+{
+    public class ChildCareStubListReader : IChildCareStubListFetcher
+    {
+        private readonly IListParser _parser;
+
+        public ChildCareStubListReader(IListParser parser)
+        {
+            _parser = parser;
+        }
+
+        public Task<IEnumerable<ChildCareStub>> Fetch(County county)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
