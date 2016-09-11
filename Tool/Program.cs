233230c58c9c049ad95b768c879b72f817abeedb
Joel Verhagen <joel.verhagen@gmail.com>
2016-09-10 19:38:55 -0700

    Remove complicated exception tracing

--- Tool/Program.cs
+++ Tool/Program.cs
@@ -84,9 +84,6 @@ namespace OdjfsScraper.Tool
             catch (Exception e)
             {
                 Logger.ErrorException("An error occurred during the execution of OdjfsScraper.Tool.", e);
-                var sb = new StringBuilder();
-                TraceExceptionMessages(sb, e, 1);
-                Logger.Error(sb.ToString().Trim());
                 return 1;
             }
         }
@@ -95,36 +92,6 @@ namespace OdjfsScraper.Tool
         {
             string version = Assembly.GetExecutingAssembly().GetInformationalVersion();
             return string.Format("OdjfsScraper/{0}", version);
-        }
-
-        private static void TraceExceptionMessages(StringBuilder sb, Exception e, int level)
-        {
-            if (e == null)
-            {
-                return;
-            }
-
-            // limit depth of recursion, since this is being run in a catch statement (gotta be careful)
-            sb.AppendFormat("{0} {1}", new string('=', level), level > 10 ? "..." : e.Message);
-            sb.AppendLine();
-            if (level > 10)
-            {
-                return;
-            }
-
-            // recurse on child exceptions
-            var ae = e as AggregateException;
-            if (ae != null)
-            {
-                foreach (Exception child in ae.InnerExceptions)
-                {
-                    TraceExceptionMessages(sb, child, level + 1);
-                }
-            }
-            else
-            {
-                TraceExceptionMessages(sb, e.InnerException, level + 1);
-            }
         }
     }
 }
