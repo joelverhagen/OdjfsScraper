43ea1aeaee22844068a1af11a29652f07b2cc4fa
Joel Verhagen <joel.verhagen@gmail.com>
2016-09-13 07:21:11 -0700

    Fix up assembly info

---
+++ Fetcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OdjfsScraper.Fetcher")]
+[assembly: AssemblyCompany("Joel Verhagen")]
+[assembly: AssemblyProduct("OdjfsScraper.Fetcher")]
+[assembly: AssemblyCopyright("Copyright (c) 2016. All rights reserved.")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyInformationalVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
